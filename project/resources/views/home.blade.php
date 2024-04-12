3432bfd7734763521c64872801679a7f4f7ba4e1
Aniballopez7 <aniballopez0708@gmail.com>
2024-04-12 13:21:27 -0400

    probando
    cambios

--- project/resources/views/home.blade.php
+++ project/resources/views/home.blade.php
@@ -13,7 +13,7 @@
                             {{ session('status') }}
                         </div>
                     @endif
-
+                        Papapu
                     {{ __('You are logged in!') }}
                 </div>
             </div>
