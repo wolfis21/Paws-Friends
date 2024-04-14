902be807b538a71a8d8f107b7391600ae73fe181
Aniballopez7 <aniballopez0708@gmail.com>
2024-04-14 13:08:25 -0400

    veterinarians
    add migration veterinarians

---
+++ project/database/migrations/2024_04_14_163631_veterinarians.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('veterinarians', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('address');
+            $table->string('phone');
+            $table->string('email');
+            $table->string('link_ref');
+            $table->string('img_ref');
+            $table->string('specialist_animals');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('veterinarians');
+    }
+};
