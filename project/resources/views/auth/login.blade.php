24356c3ec36c1bc0c03860fe4f384bf80331d912
Isaac <isaacsaado69@gmail.com>
2024-04-18 21:45:47 -0400

    cambios de app layouts

--- project/resources/views/auth/login.blade.php
+++ project/resources/views/auth/login.blade.php
@@ -1,4 +1,5 @@
-@extends('layouts.app')
+{{-- @extends('layouts.app') --}}
+@extends('layouts.headerNav')
 
 @section('content')
 <div class="container">
--- project/resources/views/auth/register.blade.php
+++ project/resources/views/auth/register.blade.php
@@ -1,4 +1,6 @@
-@extends('layouts.app')
+{{-- @extends('layouts.app') --}}
+
+@extends('layouts.headerNav')
 
 @section('content')
 <div class="container">
