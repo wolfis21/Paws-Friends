ced3399ba9ea317f58bafff4283f03ad347737a0
vargas <josevargasserrano2000@gmail.com>
2024-04-23 17:07:24 -0400

    slider

--- project/resources/views/layouts/headerNav.blade.php
+++ project/resources/views/layouts/headerNav.blade.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link rel="stylesheet" href="{{ asset ('css/styleMain.css')}}"  type="text/css"/> 
     <link rel="stylesheet" href="{{ asset ('css/app.css')}}"  type="text/css"/>
     <!-- Fonts -->
@@ -73,9 +74,10 @@
     <div class="containerMainNav">
         @yield('content')
     </div>
-
+        
     <footer>
         <p>aaaaaaaaaaaaaaaaaa</p>
     </footer>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
     </body>
 </html>
--- project/resources/views/main.blade.php
+++ project/resources/views/main.blade.php
@@ -41,7 +41,44 @@
     </div>
 
     <div id="ShowcaseQuienes">
+        <p id="txt-quienes">Paws & Friends es una organizacion comprometida con el bienestar de las mascotas, ofreciendo una amplia gama de servicos para satisfacer
+            todas las necesidades de tus peludos.
+        </p>
+    </div>
 
+    <div id="ShowcaseEquipo"> 
+        <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
+            <ol class="carousel-indicators">
+                <li data-target="#carouselExampleIndicators" data-bs-slide-to="0"></li>
+                <li data-target="#carouselExampleIndicators" data-bs-slide-to="1" class="active"></li>
+                <li data-target="#carouselExampleIndicators" data-bs-slide-to="2"></li>
+                <li data-target="#carouselExampleIndicators" data-bs-slide-to="3"></li>
+            </ol>
+            <div class="carousel-inner">
+                <div class="carousel-item active">
+                    <img class="d-block w-100" src="{{ URL::asset ('imagenes/FRAMES/FRAME D/vet 1.png')}}" alt="First slide">
+                </div>
+                <div class="carousel-item">
+                    <img class="d-block w-100" src="{{ URL::asset ('imagenes/FRAMES/FRAME D/vet 2.png')}}" alt="Second slide">
+                </div>
+                <div class="carousel-item">
+                    <img class="d-block w-100" src="{{ URL::asset ('imagenes/FRAMES/FRAME D/vet 3.png')}}" alt="Third slide">
+                </div>
+                <div class="carousel-item">
+                    <img class="d-block w-100" src="{{ URL::asset ('imagenes/FRAMES/FRAME D/vet 4.png')}}" alt="Third slide">
+                </div>
+
+                <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-bs-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="sr-only"></span>
+            </a>
+            <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-bs-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="sr-only"></span>
+            </a>
+            </div>
+            
+        </div>
     </div>
 
 @endsection
