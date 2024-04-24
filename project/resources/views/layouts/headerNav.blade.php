96d99109e4f8dabefa57df478e59ffe199b9b26b
Isaac <isaacsaado69@gmail.com>
2024-04-23 20:59:24 -0400

    ajust home

--- project/resources/views/layouts/headerNav.blade.php
+++ project/resources/views/layouts/headerNav.blade.php
@@ -1,11 +1,13 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <link rel="stylesheet" href="{{ asset ('css/styleMain.css')}}"  type="text/css"/> 
-    <link rel="stylesheet" href="{{ asset ('css/app.css')}}"  type="text/css"/>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="{{ asset('css/styleMain.css') }}" type="text/css" />
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}" type="text/css" />
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=Nunito" rel="stylesheet">
@@ -14,70 +16,79 @@
     @vite(['resources/sass/app.scss', 'resources/js/app.js'])
     <title>Paws & Friends</title>
 </head>
+
 <body>
     <nav class="nav-main-home">
         <div class="container">
             <ul id="MainNavBar">
-                <h1><img id="logoNav" src="{{ URL::asset ('imagenes/Variaciones logo.png')}}" alt="logo"/></h1>
+                <h1>
+                    <a href="/main">
+                        <img id="logoNav" src="{{ URL::asset('imagenes/Variaciones logo.png') }}" alt="logo" />
+                    </a>
+                </h1>
 
-                <li><a>Inicio</a></li>
-                <li><a>Nosotros</a></li>
-                <li><a>Servicios</a></li>
-                <li><a>Promociones</a></li>
+                <li><a href="#">Inicio</a></li>
+                <li><a href="#ShowcaseServicios">Servicios</a></li>
+                <li><a href="#ShowcaseQuienes">Nosotros</a></li>
+                <li><a href="#">Promociones</a></li>
 
-               {{--  <li><a href="{{ route('register') }}" id="registro">Registrate</a></li>
+                {{--  <li><a href="{{ route('register') }}" id="registro">Registrate</a></li>
                 <li><a href="{{ route('login') }}" id="ingreso">Iniciar Sesion</a></li> --}}
 
                 @guest
-                @if (Route::has('login'))
-                    <li>
-                        <a id="registro" href="{{ route('login') }}">{{ __('Login') }}</a>
-                    </li>
-                @endif
+                    @if (Route::has('login'))
+                        <li>
+                            <a id="registro" href="{{ route('login') }}">{{ __('Iniciar Sesión') }}</a>
+                        </li>
+                    @endif
 
-                @if (Route::has('register'))
-                    <li>
-                        <a id="ingreso" href="{{ route('register') }}">{{ __('Register') }}</a>
-                    </li>
-                @endif
-            @else
-                <li class="nav-item dropdown">
-                    <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                        {{ Auth::user()->name }}
-                    </a>
-
-                    <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
-                        
-                        <a class="btn btn-sm btn-primary" href="{{ route('user.profile') }}">
-                            <i class="fa fa-fw fa-eye"></i> Ver Perfil
+                    @if (Route::has('register'))
+                        <li>
+                            <a id="ingreso" href="{{ route('register') }}">{{ __('Registrarse') }}</a>
+                        </li>
+                    @endif
+                @else
+                    <li class="nav-item dropdown">
+                        <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button"
+                            data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                            {{ Auth::user()->name }}
                         </a>
-                        
-                        
-                        <a class="dropdown-item" href="{{ route('logout') }}"
-                           onclick="event.preventDefault();
+
+                        <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+
+                            <a class="btn btn-sm btn-primary" href="{{ route('user.profile') }}">
+                                <i class="fa fa-fw fa-eye"></i> Ver Perfil
+                            </a>
+
+
+                            <a class="dropdown-item" href="{{ route('logout') }}"
+                                onclick="event.preventDefault();
                                          document.getElementById('logout-form').submit();">
-                            {{ __('Logout') }}
-                        </a>
+                                {{ __('Logout') }}
+                            </a>
 
-                        <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                            @csrf
-                            <button type="submit">Logout</button>
-                        </form>
-                        
-                    </div>
-                </li>
-            @endguest
-                
+                            <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                @csrf
+                                <button type="submit">Logout</button>
+                            </form>
+
+                        </div>
+                    </li>
+                @endguest
+
             </ul>
         </div>
     </nav>
     <div class="containerMainNav">
         @yield('content')
     </div>
-        
+
     <footer>
         <p>aaaaaaaaaaaaaaaaaa</p>
     </footer>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
-    </body>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous">
+    </script>
+</body>
+
 </html>
--- project/resources/views/main.blade.php
+++ project/resources/views/main.blade.php
@@ -1,52 +1,52 @@
 @extends('layouts.headerNav')
 @section('content')
-    <div id="ShowcaseMain"> 
+    <div id="ShowcaseMain">
         <div>
             <a class="btn-main" href="#">Mas Informacion</a>
         </div>
     </div>
 
-    <div id="ShowcaseServicios"> 
+    <div id="ShowcaseServicios" class="services-box">
 
-        <div class="modulo-box">
-            <div>
-                <a href="a">
-                    <img class="iconosModulos" id="cuidado" src="{{ URL::asset ('imagenes/FRAMES/FRAME B/1.png')}}" alt="Cuidados">
-                </a>
-            </div>
+        <div>
+            <a href="#">
+                <img class="iconosModulos" id="cuidado" src="{{ URL::asset('imagenes/FRAMES/FRAME B/1.png') }}"
+                    alt="Cuidados">
+            </a>
         </div>
 
-        <div class="modulo-box">
-            <div>
-                <a href="b"> 
-                    <img class="iconosModulos" id="donaciones" src="{{ URL::asset ('imagenes/FRAMES/FRAME B/2.png')}}" alt="Donaciones y Adopciones">
-                </a> 
-            </div>
+        <div>
+            <a href="#">
+                <img class="iconosModulos" id="donaciones" src="{{ URL::asset('imagenes/FRAMES/FRAME B/2.png') }}"
+                    alt="Donaciones y Adopciones">
+            </a>
         </div>
-        <div class="modulo-box">
-            <div>
-                <a href="c">
-                    <img class="iconosModulos" id="rescates" src="{{ URL::asset ('imagenes/FRAMES/FRAME B/3.png')}}" alt="Rescates">
-                </a>
-            </div>
+
+        <div>
+            <a href="#">
+                <img class="iconosModulos" id="rescates" src="{{ URL::asset('imagenes/FRAMES/FRAME B/3.png') }}"
+                    alt="Rescates">
+            </a>
         </div>
-        <div class="modulo-box">
-            <div>
-                <a href="d">
-                    <img class="iconosModulos" id="tienda" src="{{ URL::asset ('imagenes/FRAMES/FRAME B/4.png')}}" alt="Catalogo">
-                </a>
-            </div>    
+
+        <div>
+            <a href="#">
+                <img class="iconosModulos" id="tienda" src="{{ URL::asset('imagenes/FRAMES/FRAME B/4.png') }}"
+                    alt="Catalogo">
+            </a>
         </div>
+
 
     </div>
 
     <div id="ShowcaseQuienes">
-        <p id="txt-quienes">Paws & Friends es una organizacion comprometida con el bienestar de las mascotas, ofreciendo una amplia gama de servicos para satisfacer
+        <p id="txt-quienes">Paws & Friends es una organizacion comprometida con el bienestar de las mascotas, ofreciendo una
+            amplia gama de servicos para satisfacer
             todas las necesidades de tus peludos.
         </p>
     </div>
 
-    <div id="ShowcaseEquipo"> 
+    <div id="ShowcaseEquipo">
         <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
             <ol class="carousel-indicators">
                 <li data-target="#carouselExampleIndicators" data-bs-slide-to="0"></li>
@@ -54,36 +54,34 @@
                 <li data-target="#carouselExampleIndicators" data-bs-slide-to="2"></li>
                 <li data-target="#carouselExampleIndicators" data-bs-slide-to="3"></li>
             </ol>
-            <div class="carousel-inner">
+            <div class="carousel-inner custom">
                 <div class="carousel-item active">
-                    <img class="d-block w-100" src="{{ URL::asset ('imagenes/FRAMES/FRAME D/vet 1.png')}}" alt="First slide">
+                    <img class="d-block w-100" src="{{ URL::asset('imagenes/FRAMES/FRAME D/vet 1.png') }}"
+                        alt="First slide">
                 </div>
                 <div class="carousel-item">
-                    <img class="d-block w-100" src="{{ URL::asset ('imagenes/FRAMES/FRAME D/vet 2.png')}}" alt="Second slide">
+                    <img class="d-block w-100" src="{{ URL::asset('imagenes/FRAMES/FRAME D/vet 2.png') }}"
+                        alt="Second slide">
                 </div>
                 <div class="carousel-item">
-                    <img class="d-block w-100" src="{{ URL::asset ('imagenes/FRAMES/FRAME D/vet 3.png')}}" alt="Third slide">
+                    <img class="d-block w-100" src="{{ URL::asset('imagenes/FRAMES/FRAME D/vet 3.png') }}"
+                        alt="Third slide">
                 </div>
                 <div class="carousel-item">
-                    <img class="d-block w-100" src="{{ URL::asset ('imagenes/FRAMES/FRAME D/vet 4.png')}}" alt="Third slide">
+                    <img class="d-block w-100" src="{{ URL::asset('imagenes/FRAMES/FRAME D/vet 4.png') }}"
+                        alt="Third slide">
                 </div>
 
                 <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-bs-slide="prev">
-                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                <span class="sr-only"></span>
-            </a>
-            <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-bs-slide="next">
-                <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                <span class="sr-only"></span>
-            </a>
+                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span class="sr-only"></span> {{-- yo esto lo quitaria --}}
+                </a>
+                <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-bs-slide="next">
+                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span class="sr-only"></span>
+                </a>
             </div>
-            
+
         </div>
     </div>
-
 @endsection
-
-
-
-    
-        
