7fee5097dd81718fbffde30485fcf74d2466118f
Aniballopez7 <aniballopez0708@gmail.com>
2024-04-25 13:06:10 -0400

    cambio para acceder a vistas Usuario , Admin

--- project/app/Http/Controllers/HomeServicesController.php
+++ project/app/Http/Controllers/HomeServicesController.php
@@ -8,6 +8,11 @@ use Illuminate\Http\Request;
 class HomeServicesController extends Controller
 {
     public function __invoke(){
+        //todo 0 = acceder a la vista del usuario, 1 = acceder a vista administrador
+        $rol = 0;
+        if ($rol === 1) {
+            return view('dashboard');
+        }
         return view('homeServices');
 
     }
--- project/resources/views/homeServices.blade.php
+++ project/resources/views/homeServices.blade.php
@@ -8,65 +8,6 @@
     <link rel="stylesheet" href="css/index.css">
 </head>
 <body>
-  {{-- //traerme el user --}}
-  <?php  
-    $isAdmin = true;
-    // $isAdmin = false;
-  ?>
-
-
-    @if ($isAdmin)
-    <nav class="navbar navbar-expand-lg bg-body-tertiary">
-        <div class="container-fluid">
-          <a class="navbar-brand" href="#">Logo</a>
-          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-          </button>
-          <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-              <li class="nav-item">
-                <a class="nav-link"  href="#">Principal</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="{{ ($isAdmin) ? route('housingAdmin') : route('housingUser') }}">Alojamiento</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="{{ ($isAdmin) ? route('vets.index') : route('vetsuser') }}">Veterinarios</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="{{ ($isAdmin) ? route('dogGroomerAdmin') : route('dogGroomerUser') }}">Peluqueria</a>
-              </li>
-            </ul>
-              <button class="btn btn-outline-danger" type="submit">Salir</button>
-          </div>
-        </div>
-      </nav>
-    <h1>SERVICE HOME</h1>
-    <p>Imagen central bonita o carrusel de imagenes</p>
-    <div id="carouselExampleInterval" class="carousel slide" data-bs-ride="carousel">
-        <div class="carousel-inner">
-          <div class="carousel-item active" data-bs-interval="1000">
-            {{-- en src pueden poner el link de la imagen --}}
-            <img src="http://placekitten.com/200/300" class="d-block " alt="...">
-          </div>
-          <div class="carousel-item" data-bs-interval="200">
-            <img src="http://placekitten.com/200/300" class="d-block " alt="...">
-          </div>
-          <div class="carousel-item">
-            <img src="http://placekitten.com/200/300" class="d-block " alt="...">
-          </div>
-        </div>
-        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="prev">
-          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-          <span class="visually-hidden">Previous</span>
-        </button>
-        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="next">
-          <span class="carousel-control-next-icon" aria-hidden="true"></span>
-          <span class="visually-hidden">Next</span>
-        </button>
-      </div>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
-    @else
       <div class="contenedor">
         <h1>Modulo de servicios</h1>
         <p>Ofrecemos estos servicios</p>
@@ -85,6 +26,5 @@
           </div>
         </div>
       </div>
-    @endif
 </body>
 </html>
