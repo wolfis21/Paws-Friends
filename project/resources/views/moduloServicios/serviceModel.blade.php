eefff86153db1cede3b5ced814290100cb46e54e
Aniballopez7 <aniballopez0708@gmail.com>
2024-04-27 13:37:08 -0400

    update front

--- project/resources/views/moduloServicios/serviceModel.blade.php
+++ project/resources/views/moduloServicios/serviceModel.blade.php
@@ -3,22 +3,22 @@
 @vite(['resources/css/moduloServicios/user/index.css'])
 @section('content')
     <div class="contenedor">
-        <h1>Modulo de servicios</h1>
-        <p>Ofrecemos estos servicios</p>
+        <h1 class="title-page">Modulo de servicios</h1>
+        <h2 class="texto">Nuestros servicios</h2>
         <div class="cards">
-            <div class="card">
+            <div class="card-img2">
                 <a href="{{route('Veterinario')}}">
                     <img src="https://images.pexels.com/photos/6235225/pexels-photo-6235225.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="">
                     <div class="pservicio">Veterinarios</div>
                   </a>
             </div>
-            <div class="card">
+            <div class="card-img2">
                 <a href="{{route('housingUser')}}">
                     <img src="https://images.unsplash.com/photo-1535126320463-c5c1b26b3e66?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D">
                     <div class="pservicio">Alojamiento</div>
                   </a>
             </div>
-            <div class="card">
+            <div class="card-img2">
                 <a href="{{route('dogGroomerUser')}}">
                     <img src="https://plus.unsplash.com/premium_photo-1664297694687-570b7f392906?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D">
                     <div class="pservicio">Peluqueria</div>
