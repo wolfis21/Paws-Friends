e0296deb30065741bcf2cf9ce199075a77bd5d05
Aniballopez7 <aniballopez0708@gmail.com>
2024-04-29 11:41:45 -0400

    estilos dashboard

--- project/resources/views/moduloServicios/dashboard.blade.php
+++ project/resources/views/moduloServicios/dashboard.blade.php
@@ -4,16 +4,107 @@
 
 
 @section('content_header')
-    <h1>Dashboard Modulo 1</h1>
+    <h3 class="page__heading">Dashboard</h3>
 @stop
 
 @section('content')
+    <section class="section">
+        <div class="section-body">
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="card bg-dark">
+                        <div class="card-body">
+                            <div class="row">
+                                <div class="col-md-4 col-xl-4">
 
+                                    <div class="card bg-c-blue order-card">
+                                        <div class="card-block">
+                                            <h5>Usuarios</h5>
+                                            @php
+                                                use App\Models\User;
+                                                $cant_usuarios = User::count();
+                                            @endphp
+                                            <h2 class="text-right"><i
+                                                    class="fa fa-users f-left"></i><span>{{ $cant_usuarios }}</span></h2>
+                                            <p class="m-b-0 text-right"><a href="/usuarios" class="text-white">Ver más</a>
+                                            </p>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div class="col-md-4 col-xl-4">
+                                    <div class="card bg-c-green order-card">
+                                        <div class="card-block">
+                                            <h5>Veterinarios</h5>
+                                            @php
+                                                use App\Models\moduloServicios\Veterinarian;
+                                                $cant_Vets = Veterinarian::count();
+                                            @endphp
+                                            <h2 class="text-right"><i
+                                                    class="fas fa-fw fa-dog f-left"></i><span>{{ $cant_Vets }}</span>
+                                            </h2>
+                                            <p class="m-b-0 text-right"><a href="/admin/Veterinarians"
+                                                    class="text-white">Ver más</a></p>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-md-4 col-xl-4">
+                                    <div class="card bg-c-pink order-card">
+                                        <div class="card-block">
+                                            <h5>Alojamientos</h5>
+                                            @php
+                                                use App\Models\moduloServicios\Housing;
+                                                $cant_Housing = Housing::count();
+                                            @endphp
+                                            <h2 class="text-right"><i
+                                                    class="fas fa-fw fa-home f-left"></i><span>{{ $cant_Housing }}</span>
+                                            </h2>
+                                            <p class="m-b-0 text-right"><a href="/admin/housingsAdmin"
+                                                    class="text-white">Ver más</a></p>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-md-4 col-xl-4">
+                                    <div class="card bg-c-orange order-card">
+                                        <div class="card-block">
+                                            <h5>Peluquerias</h5>
+                                            @php
+                                                use App\Models\moduloServicios\Dog_groomer;
+                                                $cant_Peluqueria = Dog_groomer::count();
+                                            @endphp
+                                            <h2 class="text-right"><i
+                                                    class="fas fa-fw fa-paw f-left"></i><span>{{ $cant_Peluqueria }}</span>
+                                            </h2>
+                                            <p class="m-b-0 text-right"><a href="/admin/DogGroomersAdmin" class="text-white">Ver más</a></p>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-md-4 col-xl-4">
+                                    <div class="card bg-c-purple order-card">
+                                        <div class="card-block">
+                                            <h5>Comentarios</h5>
+                                            @php
+                                                use App\Models\moduloServicios\Comment;
+                                                $cant_Comentarios = Comment::count();
+                                            @endphp
+                                            <h2 class="text-right"><i
+                                                    class="fa fa-comment f-left"></i><span>{{ $cant_Comentarios }}</span>
+                                            </h2>
+                                            <p class="m-b-0 text-right"><ico class="text-black"></ico></p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
 @stop
 
 @section('css')
-    <link rel="stylesheet" href="/css/admin_custom.css">
-    <link rel="stylesheet" href="{{asset('ccs/index.ccs')}}">
+    @vite(['resources/css/moduloServicios/admin/index.css'])
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.3.0/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://cdn.datatables.net/2.0.5/css/dataTables.bootstrap5.css">
     <link rel="stylesheet" href="https://cdn.datatables.net/responsive/3.0.2/css/responsive.bootstrap5.css">
@@ -27,164 +118,9 @@
     <script src="https://cdn.datatables.net/2.0.5/js/dataTables.bootstrap5.js"></script>
     <script src="https://cdn.datatables.net/responsive/3.0.2/js/dataTables.responsive.js"></script>
     <script src="https://cdn.datatables.net/responsive/3.0.2/js/responsive.bootstrap5.js"></script>
+    @vite(['moduloServicios/js/admin/dataTables/scriptDatatables.js'])
     {{-- <script src="{{asset('moduloServicios/js/admin/dataTables/jquery-3.7.1.js')}}"></script>
     <script src="{{asset('moduloServicios/js/admin/dataTables/bootstrap.bundle.min.js')}}"></script>
     <script src="{{asset('moduloServicios/js/admin/dataTables/dataTables.js')}}"></script>
     <script src="{{asset('moduloServicios/js/admin/dataTables/dataTables.bootstrap5.js')}}"></script> --}}
-    <script>
-        new DataTable('#Veterinarios', {
-            responsive: true,
-            autoWith: false,
-            "language": {
-                "lengthMenu": "Mostrar" +`
-                <select class="custom-select custom-select-sm form-control form-control-sm">
-                    <option value = '5'>5</option>
-                    <option value = '10'>10</option>
-                    <option value = '25'>25</option>
-                    <option value = '100'>100</option>
-                    <option value = '-1'>Todos</option>
-                    </select>
-                `+ 
-                "registros por pagina",
-                "zeroRecords": "No se encontro nada - disculpa",
-                "info": "Mostrando la pagina _PAGE_ de _PAGES_",
-                "infoEmpty": "No hay registros disponibles",
-                "infoFiltered": "(Filtrado de _MAX_ registros totales)",
-                "search": "Buscar:",
-                "paginate": {
-                    "next": "Siguiente",
-                    "previous": "Anterior"
-                }
-            }
-        });
-        new DataTable('#ComentariosVets',{
-            responsive: true,
-            autoWith: false,
-            "language": {
-                "lengthMenu": "Mostrar" +`
-                <select class="custom-select custom-select-sm form-control form-control-sm">
-                    <option value = '5'>5</option>
-                    <option value = '10'>10</option>
-                    <option value = '25'>25</option>
-                    <option value = '100'>100</option>
-                    <option value = '-1'>Todos</option>
-                    </select>
-                `+ 
-                "registros por pagina",
-                "zeroRecords": "No se encontro nada - disculpa",
-                "info": "Mostrando la pagina _PAGE_ de _PAGES_",
-                "infoEmpty": "No hay registros disponibles",
-                "infoFiltered": "(Filtrado de _MAX_ registros totales)",
-                "search": "Buscar:",
-                "paginate": {
-                    "next": "Siguiente",
-                    "previous": "Anterior"
-                }
-            }
-        });
-
-        new DataTable('#Alojamiento',{
-            responsive: true,
-            autoWith: false,
-            "language": {
-                "lengthMenu": "Mostrar" +`
-                <select class="custom-select custom-select-sm form-control form-control-sm">
-                    <option value = '5'>5</option>
-                    <option value = '10'>10</option>
-                    <option value = '25'>25</option>
-                    <option value = '100'>100</option>
-                    <option value = '-1'>Todos</option>
-                    </select>
-                `+ 
-                "registros por pagina",
-                "zeroRecords": "No se encontro nada - disculpa",
-                "info": "Mostrando la pagina _PAGE_ de _PAGES_",
-                "infoEmpty": "No hay registros disponibles",
-                "infoFiltered": "(Filtrado de _MAX_ registros totales)",
-                "search": "Buscar:",
-                "paginate": {
-                    "next": "Siguiente",
-                    "previous": "Anterior"
-                }
-            }
-        });
-
-        new DataTable('#AlojamientoComments',{
-            responsive: true,
-            autoWith: false,
-            "language": {
-                "lengthMenu": "Mostrar" +`
-                <select class="custom-select custom-select-sm form-control form-control-sm">
-                    <option value = '5'>5</option>
-                    <option value = '10'>10</option>
-                    <option value = '25'>25</option>
-                    <option value = '100'>100</option>
-                    <option value = '-1'>Todos</option>
-                    </select>
-                `+ 
-                "registros por pagina",
-                "zeroRecords": "No se encontro nada - disculpa",
-                "info": "Mostrando la pagina _PAGE_ de _PAGES_",
-                "infoEmpty": "No hay registros disponibles",
-                "infoFiltered": "(Filtrado de _MAX_ registros totales)",
-                "search": "Buscar:",
-                "paginate": {
-                    "next": "Siguiente",
-                    "previous": "Anterior"
-                }
-            }
-        });
-
-        new DataTable('#Peluqueria',{
-            responsive: true,
-            autoWith: false,
-            "language": {
-                "lengthMenu": "Mostrar" +`
-                <select class="custom-select custom-select-sm form-control form-control-sm">
-                    <option value = '5'>5</option>
-                    <option value = '10'>10</option>
-                    <option value = '25'>25</option>
-                    <option value = '100'>100</option>
-                    <option value = '-1'>Todos</option>
-                    </select>
-                `+ 
-                "registros por pagina",
-                "zeroRecords": "No se encontro nada - disculpa",
-                "info": "Mostrando la pagina _PAGE_ de _PAGES_",
-                "infoEmpty": "No hay registros disponibles",
-                "infoFiltered": "(Filtrado de _MAX_ registros totales)",
-                "search": "Buscar:",
-                "paginate": {
-                    "next": "Siguiente",
-                    "previous": "Anterior"
-                }
-            }
-        });
-
-        new DataTable('#PeluqueriaComments',{
-            responsive: true,
-            autoWith: false,
-            "language": {
-                "lengthMenu": "Mostrar" +`
-                <select class="custom-select custom-select-sm form-control form-control-sm">
-                    <option value = '5'>5</option>
-                    <option value = '10'>10</option>
-                    <option value = '25'>25</option>
-                    <option value = '100'>100</option>
-                    <option value = '-1'>Todos</option>
-                    </select>
-                `+ 
-                "registros por pagina",
-                "zeroRecords": "No se encontro nada - disculpa",
-                "info": "Mostrando la pagina _PAGE_ de _PAGES_",
-                "infoEmpty": "No hay registros disponibles",
-                "infoFiltered": "(Filtrado de _MAX_ registros totales)",
-                "search": "Buscar:",
-                "paginate": {
-                    "next": "Siguiente",
-                    "previous": "Anterior"
-                }
-            }
-        });
-    </script>
 @stop
--- project/vite.config.js
+++ project/vite.config.js
@@ -9,12 +9,14 @@ export default defineConfig({
 
                 //todo css de mainView
                 'resources/css/moduloServicios/user/index.css',
+                'resources/css/moduloServicios/admin/index.css',
 
                 //todo dataTables
                 //?CSS
                 // 'resources/css/moduloServicios/admin/dataTables/bootstrap.min.css',
                 // 'resources/css/moduloServicios/admin/dataTables/dataTables.bootstrap5.min.css',
-                
+                //?JS
+                'moduloServicios/js/admin/dataTables/scriptDatatables.js',
                 //todo js
                 'resources/js/app.js',
             ],
