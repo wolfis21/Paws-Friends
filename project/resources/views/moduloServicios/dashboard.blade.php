ef883e58ab4b6ce007a659ff126938112f196a63
Aniballopez7 <aniballopez0708@gmail.com>
2024-05-04 01:36:36 -0400

    estilos blanco

--- project/resources/views/moduloServicios/dashboard.blade.php
+++ project/resources/views/moduloServicios/dashboard.blade.php
@@ -103,7 +103,7 @@
 @section('css')
     @vite(['resources/css/moduloServicios/admin/index.css'])
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.3.0/css/bootstrap.min.css">
-    <link rel="stylesheet" href="https://cdn.datatables.net/2.0.5/css/dataTables.bootstrap5.css">
+    <link rel="stylesheet" href="https://cdn.datatables.net/2.0.6/css/dataTables.bootstrap5.css">
     <link rel="stylesheet" href="https://cdn.datatables.net/responsive/3.0.2/css/responsive.bootstrap5.css">
 @stop
 
