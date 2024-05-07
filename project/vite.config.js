56d5638640c60cadbaaddc52c3641eaba4c3de47
Aniballopez7 <aniballopez0708@gmail.com>
2024-05-06 20:14:29 -0400

    funciona

--- project/vite.config.js
+++ project/vite.config.js
@@ -5,7 +5,6 @@ export default defineConfig({
     plugins: [
         laravel({
             input: [
-<<<<<<< HEAD
                 // todo css de navMenu
 
                 //todo css de mainView
@@ -16,10 +15,10 @@ export default defineConfig({
                 'resources/js/app.js',
                 "resources/sass/app.scss",
                 "resources/js/app.js",
-=======
+
                 'resources/sass/app.scss',
                 'resources/js/app.js',
->>>>>>> origin/module-3
+                // origin/module-3
             ],
             refresh: true,
         }),
