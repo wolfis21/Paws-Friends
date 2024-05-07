d3cb74791508706d6dde27096d41947fc448434a
Aniballopez7 <aniballopez0708@gmail.com>
2024-05-07 15:16:14 -0400

    vistas perfil

--- project/resources/views/profile/index.blade.php
+++ project/resources/views/profile/index.blade.php
@@ -6,6 +6,7 @@
         <div for="photo_user" class="name">photo_user: </div>
     </div>
     <div class="info-User">
+<<<<<<< Updated upstream
         <div class="card-space">
             <div for="Nombre" class="name">Nombre: </div>
             <div class="user-info">{{$user->name}}</div>
@@ -28,10 +29,41 @@
         </div>
         <div class="card-space">
             <div for="email" class="name">Email:</div>
-            <div class="user-info">{{$user->email}}</div>
+=======
+        <div class="title">
+            <h2>Informacion del Usuario</h2>
         </div>
         <div class="card-space">
-            <a class="btn bg-cyan" href="{{route('userEdit', $user->id)}}">Editar</a>
+            <label for="Nombre" class="label">Nombre </label>
+            <div class="user-info">{{$user->name}}</div>
+        </div>
+        <div class="card-space">
+            <label for="last_name" class="label">Apellido </label>
+            <div class="user-info">{{$user->last_name}}</div>
+        </div>
+        <div class="card-space">
+            <label for="dni" class="label">Dni </label>
+            <div class="user-info">{{$user->dni}}</div>
+        </div>
+        <div class="card-space">
+            <label for="phone" class="label">Phone </label>
+            <div class="user-info">{{$user->phone}}</div>
+        </div>
+        <div class="card-space ae">
+            <label for="address" class="label">Adress</label>
+            <div class="user-info">{{$user->address}}</div>
+        </div>
+                    <div class="card-space">
+                <label for="photo_user" class="label">photo_user </;>
+                    <input class="user-info" type="text" value="{{ $user->photo_user }}">
+            </div>
+        <div class="card-space ae">
+            <label for="email" class="label">Email</label>
+>>>>>>> Stashed changes
+            <div class="user-info">{{$user->email}}</div>
+        </div>
+        <div class="card-space btn-link">
+            <a class="btna" href="{{route('userEdit', $user->id)}}">Editar</a>
         </div>
     </div>
 </div>
