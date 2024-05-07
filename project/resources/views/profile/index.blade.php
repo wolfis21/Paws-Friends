0e7b737a0a43c4708e31cf21b24f650c1253689e
Aniballopez7 <aniballopez0708@gmail.com>
2024-05-07 13:46:39 -0400

    estilos view

--- project/resources/views/profile/index.blade.php
+++ project/resources/views/profile/index.blade.php
@@ -2,32 +2,32 @@
 @vite('resources/css/profile/index.css')
 @section('content')
 <div class="container2">
-    <div class="card-space">
-        <div for="photo_user" class="name">photo_user: </div>
-    </div>
     <div class="info-User">
         <div class="card-space">
-            <div for="Nombre" class="name">Nombre: </div>
+            <div for="photo_user" class="name">photo_user: </div>
+        </div>
+        <div class="card-space">
+            <label for="Nombre" class="name">Nombre: </label>
             <div class="user-info">{{$user->name}}</div>
         </div>
         <div class="card-space">
-            <div for="last_name" class="name">Apellido: </div>
+            <label for="last_name" class="name">Apellido: </label>
             <div class="user-info">{{$user->last_name}}</div>
         </div>
         <div class="card-space">
-            <div for="dni" class="name">Dni: </div>
+            <label for="dni" class="name">Dni: </label>
             <div class="user-info">{{$user->dni}}</div>
         </div>
         <div class="card-space">
-            <div for="phone" class="name">Phone: </div>
+            <label for="phone" class="name">Phone: </label>
             <div class="user-info">{{$user->phone}}</div>
         </div>
         <div class="card-space">
-            <div for="address" class="name">Adress:</div>
+            <label for="address" class="name">Adress:</label>
             <div class="user-info">{{$user->address}}</div>
         </div>
         <div class="card-space">
-            <div for="email" class="name">Email:</div>
+            <label for="email" class="name">Email:</label>
             <div class="user-info">{{$user->email}}</div>
         </div>
         <div class="card-space">
