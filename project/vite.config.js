3518787b35b9cb9f365d8c492881d65adf6d1cb6
Aniballopez7 <aniballopez0708@gmail.com>
2024-05-06 21:36:47 -0400

    papapu

--- project/vite.config.js
+++ project/vite.config.js
@@ -5,10 +5,6 @@ export default defineConfig({
     plugins: [
         laravel({
             input: [
-<<<<<<< HEAD
-
-=======
->>>>>>> ae9472e776cb0b3ff59b109646cf1b63f3a1c063
                 // todo css de navMenu
 
                 //todo css de mainView
@@ -22,10 +18,6 @@ export default defineConfig({
 
                 'resources/sass/app.scss',
                 'resources/js/app.js',
-<<<<<<< HEAD
-=======
-                // origin/module-3
->>>>>>> ae9472e776cb0b3ff59b109646cf1b63f3a1c063
             ],
             refresh: true,
         }),
