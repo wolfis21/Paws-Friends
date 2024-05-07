4bd3c82358c953fd955b859f2149f74f11deecfc
Aniballopez7 <aniballopez0708@gmail.com>
2024-05-07 18:05:52 -0400

    login

--- project/resources/views/auth/login.blade.php
+++ project/resources/views/auth/login.blade.php
@@ -2,9 +2,9 @@
 
 {{-- @extends('layouts.app') --}}
 @extends('layouts.headerNav')
-
+@vite('resources/css/login/loginUser.css')
 @section('content')
-<div class="container">
+{{-- <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
@@ -70,6 +70,43 @@
                     </form>
                 </div>
             </div>
+        </div>
+    </div>
+</div> --}}
+<div class="content">
+    <div class="info-User">
+        <div class="register-welcome">
+           <div class="welcome">
+               <h3>Bienvenido a </h3>
+           </div>
+           <div class="logo">
+               <img class="logo-register" src="{{ URL::asset('imagenes/Variaciones logo.png') }}" alt="logo" />
+           </div>
+           <div class="register">
+                <a href="#">¿No tienes una cuenta?
+                Registrate</a>
+           </div>
+        </div>
+        <div class="title">
+            <h2>Inicia Sesion</h2>
+        </div>
+        <div class="card-space">
+            <label for="Nombre" class="label">Ingresa tu correo electronico</label>
+            <input type="text" class="user-info" placeholder="Correo electronico"></input>
+        </div>
+        <div class="card-space">
+            <label for="last_name" class="label">Ingresa tu contraseña </label>
+            <input type="password" class="user-info" placeholder="Contraseña"></input>
+        </div>
+        <div class="remenber">
+            <div class="input-remeber">
+                <input type="checkbox" class="user-info" placeholder="Recuerdame">
+                <p class="texto">Recuerdame</p>   
+            </div>
+            <a href="#">¿Olvidaste tu contraseña?</a>
+        </div>
+        <div class="card-space btn-link">
+            <button type="submit" class="btnE">Iniciar sesion</button>
         </div>
     </div>
 </div>
