54f9621baac5479bd8908e3f770282c66b2d5f1f
Aniballopez7 <aniballopez0708@gmail.com>
2024-05-07 01:44:58 -0400

    al iniciar con admin redirige al crub

--- project/app/Http/Controllers/HomeController.php
+++ project/app/Http/Controllers/HomeController.php
@@ -14,7 +14,7 @@ class HomeController extends Controller
     } */
     
     public function __invoke(){
-       return view('moduloServicios.dashboard');
+       return view('layouts.dashboard');
    }
     /**
      * Show the application dashboard.
