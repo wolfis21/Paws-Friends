b07d2697de979fdfdd12aa2148a49151e972307f
Aniballopez7 <aniballopez0708@gmail.com>
2024-05-10 13:25:21 -0400

    estilos

--- project/app/Http/Controllers/moduloServicios/VeterinarianController.php
+++ project/app/Http/Controllers/moduloServicios/VeterinarianController.php
@@ -103,7 +103,7 @@ class VeterinarianController extends Controller
             'phone' => 'required||alpha_num|min_digits:11',
             'email' => 'required|email',
             'link_ref' => 'nullable',
-            'img_ref' => 'nullable|image|mimes:jpeg,png,jpg|max:2048',
+            'img_ref' => 'nullable|image|mimes:jpeg,png,jpg,webp|max:2048',
             'specialist_animals' => 'required|string',
             'all_puntuation' => 'nullable',
             'puntuation' => 'nullable',
