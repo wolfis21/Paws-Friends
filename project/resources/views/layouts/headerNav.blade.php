bf3d5019b499e80da835151752457243f2ae6f2e
Aniballopez7 <83249435+Aniballopez7@users.noreply.github.com>
2024-05-14 15:57:00 -0400

    Update headerNav.blade.php

--- project/resources/views/layouts/headerNav.blade.php
+++ project/resources/views/layouts/headerNav.blade.php
@@ -6,7 +6,6 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="{{ asset('css/styleMain.css') }}" type="text/css" />
-    <link rel="stylesheet" href="{{ asset('css/app.css') }}" type="text/css" />
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.3.0/css/bootstrap.min.css">
     <link rel="icon" type="image/x-icon" href="{{asset('imagenes/icono.ico')}}">
     <!-- Fonts -->
