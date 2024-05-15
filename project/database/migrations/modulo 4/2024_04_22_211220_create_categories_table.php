9381557014df50abbd298ffb2b5cee8385fec0d0
MariaFernanda264 <mafern2604@gmail.com>
2024-05-15 14:28:03 -0700

    modificacion nombre tabla categories

--- project/database/migrations/modulo 4/2024_04_22_211220_create_categories_table.php
+++ project/database/migrations/modulo 4/2024_04_22_211220_create_categories_table.php
@@ -9,6 +9,7 @@ return new class extends Migration
     /**
      * Run the migrations.
      */
+    //categories_product, tabla para crear categorias de productos
     public function up(): void
     {
         Schema::create('categories', function (Blueprint $table) {
