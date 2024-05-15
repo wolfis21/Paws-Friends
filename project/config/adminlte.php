1f472cc6e0ffae3ba3bd7aaed4479366809911a9
Henry Barreto <hmbs2892001@gmail.com>
2024-05-15 18:01:54 -0400

    Update adminlte.php

--- project/config/adminlte.php
+++ project/config/adminlte.php
@@ -196,7 +196,7 @@ return [
     'classes_content_wrapper' => '',
     'classes_content_header' => '',
     'classes_content' => '',
-    'classes_sidebar' => 'sidebar-dark-bg-white elevation-4',
+    'classes_sidebar' => 'sidebar-dark-white elevation-4',
     'classes_sidebar_nav' => '',
     'classes_topnav' => 'navbar-dark navbar-dark',
     'classes_topnav_nav' => 'navbar-expand',
