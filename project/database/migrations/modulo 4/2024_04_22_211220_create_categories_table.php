0440ef48b79471a0ad197dc1a648da673e2b21f3
MariaFernanda264 <mafern2604@gmail.com>
2024-05-15 13:46:32 -0700

    modificacion modelo categoria

--- project/database/migrations/modulo 4/2024_04_22_211220_create_categories_table.php
+++ project/database/migrations/modulo 4/2024_04_22_211220_create_categories_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('categories', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->text('descriptions');
+            $table->text('description');
             $table->string('img_ref')->nullable();
             $table->timestamps();
         });
