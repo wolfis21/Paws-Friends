bc0948645162725a3200f72378aeb60f33364314
vargas <josevargasserrano2000@gmail.com>
2024-05-15 23:13:55 -0400

    .....

--- project/resources/views/auth/register.blade.php
+++ project/resources/views/auth/register.blade.php
@@ -1,11 +1,65 @@
 @extends('layouts.headerNav')
-<<<<<<< HEAD
-
-=======
 @vite('resources/css/register/registroUser.css')
->>>>>>> 5c9581026f814ce8eca086111a8c7c3af65c7269
+
 @section('content')
     <div class="container-register">
+        <fieldset >
+            <form class="box-registre">
+                <div class="register-welcome">
+                    <div class="welcome">
+                        <h3 class="text-h3">Bienvenido a </h3>
+                    </div>
+                    <div class="logo">
+                        <img class="logo-register" src="{{ URL::asset('imagenes/Variaciones-logo.png') }}" alt="logo" />
+                    </div>
+                    <div class="register">
+                        <span>¿Ya tienes una cuenta?</span><a href="#">Inicia sesion</a>
+                    </div>
+                </div>    
+                <div class="title">
+                    <h2>Registrate</h2>
+                </div>
 
+                <div class="f-left">
+                    <div class="card-space" >
+                        <label>Nombre</label>
+                        <input class="user-info" placeholder="Nombre"/>
+                    </div>
+                    <div class="card-space">
+                        <label>Cedula</label>
+                        <input class="user-info" placeholder="Cedula"/>
+                    </div>
+                    <div class="card-space">
+                        <label>Direccion</label>
+                        <input class="user-info" placeholder="Direccion"/>
+                    </div>
+                    <div class="card-space">
+                    <label>Apellido</label>
+                    <input class="user-info" placeholder="Apellido"/>
+                </div>
+                <div class="card-space">
+                    <label>Telefono Movil</label>
+                    <input class="user-info" placeholder="Telefono Movil"/>
+                </div>
+                <div class="card-space">
+                    <label>Foto</label>
+                    <input class="user-info" placeholder="Foto de Perfil"/>
+                </div>
+                </div>
+                
+                
+                <div class="card-foot">
+                    <label>Ingresa correo electronico</label>
+                    <input class="user-info" placeholder="Correo Electronico"/>
+                </div>
+                <div class="card-foot">
+                    <label>Ingresa tu contrasena</label>
+                    <input class="user-info" placeholder="Contrasena de la Cuenta"/>
+                </div>
+                <div class="card-space btn-link">
+                    <button type="submit" class="btnE">{{ __('Registrate') }}</button>
+                </div>
+            </form>
+        </fieldset>
     </div>
 @endsection
