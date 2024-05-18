e9ac7ddc40489798139910d7289bafc06878fd7b
MariaFernanda264 <mafern2604@gmail.com>
2024-05-18 10:03:53 -0700

    modelos de product_has_puntuation y shop_has_puntuation

---
+++ project/app/Models/moduloCatalogo/shops_has_puntuation.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models\moduloCatalogo;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class shops_has_puntuation extends Model
+{
+    protected $table = 'shop_has_puntuations';
+
+    protected $fillable = [
+        'puntuations_id',
+        'shops_id',
+        'created_at',
+        'updated_at',
+    ];
+    public function shops(){
+        return $this->belongsTo(Shop::class, 'shops_id', 'id');
+    }
+
+    public function puntuations(){
+        return $this->belongsTo(Puntuations::class, 'puntuations_id', 'id');
+    }
+
+    use HasFactory;
+}
