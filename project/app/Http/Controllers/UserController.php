989411eb0ff0f50378679b8fdcf325ad68680d14
Aniballopez7 <83249435+Aniballopez7@users.noreply.github.com>
2024-05-18 19:44:00 -0400

    verificacion al eliminar admin

--- project/app/Http/Controllers/UserController.php
+++ project/app/Http/Controllers/UserController.php
@@ -9,9 +9,10 @@ use App\Models\User;
 
 class UserController extends Controller
 {
-    public function show(string $id){
+    public function show(string $id)
+    {
         $user = Auth::user();
-        
+
         // $img = base64_encode(file_get_contents(public_path('storage/' . $user->photo_user)));
 
         return view('profile.index', compact('user'));
@@ -23,16 +24,17 @@ class UserController extends Controller
         return view('profile.edit', compact('user'));
     }
 
-    public function updateUser(Request $request, string $id){
+    public function updateUser(Request $request, string $id)
+    {
         $user = User::findOrFail($id);
-    
-        $userData = $request->except('photo_user','photo_dni','photo_rif');
-        
+
+        $userData = $request->except('photo_user', 'photo_dni', 'photo_rif');
+
         if ($request->hasFile('photo_dni') && $request->hasFile('photo_rif') && $request->hasFile('photo_user')) {
             $imgDni = $request->file('photo_dni')->store('docs', 'public');
             $imgRif = $request->file('photo_rif')->store('docs', 'public');
             $imgPhoto = $request->file('photo_user')->store('docs', 'public');
-            
+
             $userData['photo_user'] = $imgPhoto;
             $userData['photo_dni'] = $imgDni;
             $userData['photo_rif'] = $imgRif;
@@ -41,26 +43,29 @@ class UserController extends Controller
             $userData['photo_dni'] = null;
             $userData['photo_rif'] = null;
         }
-    
+
         $user->update($userData);
-        
+
         return view('main');
     }
-    
-    public function allAdmin(){
+
+    public function allAdmin()
+    {
         $admins = User::where('rols_id', 1)->get();
         return view('moduloUsers.admin.index', compact('admins'));
     }
-    public function allUser(){
+    public function allUser()
+    {
         $customers = User::where('rols_id', 2)->get();
         return view('moduloUsers.user.index', compact('customers'));
     }
-    public function createAdmin(){
+    public function createAdmin()
+    {
         return view('moduloUsers.admin.create');
     }
     public function createForm(Request $request)
-    {   
-        if ($request['photo_dni'] && $request['photo_rif']&& $request['photo_user']) {
+    {
+        if ($request['photo_dni'] && $request['photo_rif'] && $request['photo_user']) {
             $imgPhoto = $request['photo_user'];
             $imgDni = $request['photo_dni'];
             $imgRif = $request['photo_rif'];
@@ -87,11 +92,16 @@ class UserController extends Controller
             'rols_id' => $request['rols_id'],
         ]);
         return $this->allAdmin();
-
     }
     public function destroyAdmin(string $id)
     {
-        User::find($id)->delete();
+        $user = User::find($id);
+        if (Auth::id() === $user->id) {
+            return redirect()->back(); 
+        } else {
+            $user = User::findOrFail($id);
+            $user->delete();
+        }
         return $this->allAdmin();
     }
 }
