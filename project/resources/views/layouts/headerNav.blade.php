a75a525f0789bfcdbd2f2f5195a22034821f1328
Aniballopez7 <83249435+Aniballopez7@users.noreply.github.com>
2024-05-21 22:51:39 -0400

    Update headerNav.blade.php

--- project/resources/views/layouts/headerNav.blade.php
+++ project/resources/views/layouts/headerNav.blade.php
@@ -12,7 +12,6 @@
     <link rel="stylesheet" href="{{ asset('css/styleMain.css') }}" type="text/css" />
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.3.0/css/bootstrap.min.css">
     <link rel="icon" type="image/x-icon" href="{{ asset('imagenes/icono.ico') }}">
-   
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=Nunito" rel="stylesheet">
